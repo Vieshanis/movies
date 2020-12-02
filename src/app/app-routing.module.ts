71ec071cf94cb81574a2e3bf8c2af803604552c2
viesturs <viesturs.berzins@hotmail.com>
2020-12-02 21:41:20 +0200

    Formating fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) }, { path: 'movie', loadChildren: () => import('./movie/movie.module').then(m => m.MovieModule) }, { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module')
+      .then(m => m.SearchModule)
+  },
+  {
+    path: 'movie',
+    loadChildren: () => import('./movie/movie.module')
+      .then(m => m.MovieModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module')
+      .then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
